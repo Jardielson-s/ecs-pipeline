4b901582606c509ff0a55bd44485d327bd4dab6d
jardielson <jardielson.ferreira@seedz.ag>
2024-01-01 12:08:42 -0300

    feat: added get random message route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('message-random')
+  getNewMessageRandom() {
+    return `Message random: ${Math.random()}`;
+  }
 }
